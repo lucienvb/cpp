6486fbcf4eaf179cacf036b42d08d89899f9f154
lvan-bus <lucienvanbussel@gmail.com>
2023-06-27 13:38:09 +0200

    playing with a draft for the Contact and Phonebook class

--- ex01/SRC/main.cpp
+++ ex01/SRC/main.cpp
@@ -1,18 +1,28 @@
 #include <phonebook.hpp>
 
-Contact::Contact(const std::string& f, const std::string& l, const std::string& n,
-					std::uint32_t p, std::string d) : first_name(f), last_name(l),
-					nick_name(n), phone_number(p), darkest_secret(d) {}
-
-void	Contact::sayHello() {
-	std::cout << "Hello, my name is " << first_name << " " << last_name << ", some people call me "
-	<< nick_name << ". You can reach me on: " << phone_number << ". My darkest secret is: "
-	<< darkest_secret << "." << std::endl;
-}
+#include <iostream>
+#include <string>
 
 int	main(void)
 {
-	Contact	contact("Han", "van Camps", "Lulhan", 6123978, "pruukske");
-	contact.sayHello();
+    std::string line;
+	Phonebook	pb;
+
+//	Contact	contact("Han", "van Camps", "Lulhan", "0612397813", "pruukske");
+	while (1)
+	{
+		std::cout << "Choose one of the following commands: ADD / SEARCH / EXIT" << std::endl;
+		std::getline(std::cin, line);
+		if (line.compare("ADD") == 0)
+			pb.addContact();
+//			contact.sayHello();
+		else if (line.compare("SEARCH") == 0)
+//			pb.printContact();
+			std::cout << "SEARCH";
+		else if (line.compare("EXIT") == 0)
+			break ;
+		else
+			std::cout << "Invalid command. ";
+	}
 	return (0);
 }
--- ex01/include/phonebook.hpp
+++ ex01/include/phonebook.hpp
@@ -4,27 +4,116 @@
 #include <string>
 #include <iostream>
 
-class Contact {
-	private:
-		std::string		first_name;
-		std::string		last_name;
-		std::string		nick_name;
-		std::uint64_t	phone_number;
-		std::string		darkest_secret;
+# define LIST_SIZE 8
 
+class Contact {
 	public:
-		Contact(const std::string& f, const std::string& l, const std::string& n,
-				std::uint32_t p, std::string d);
-		void sayHello();
+		Contact() {
+			firstName = "";
+			lastName = "";
+			nickName = "";
+			phoneNumber = "";
+			darkestSecret = "";
+		}
+
+		void	sayHello() {
+			std::cout << "Hello, my name is " << firstName << " " << lastName << ", some people call me "
+					  << nickName << ". You can reach me on: " << phoneNumber << ". My darkest secret is: "
+					  << darkestSecret << "." << std::endl;
+		};
+
+		std::string	getFirstName() const {
+			return (firstName);
+		}
+		std::string	getLastName() const {
+			return (lastName);
+		}
+		std::string	getNickName() const {
+			return (nickName);
+		}
+		std::string	getPhoneNumber() const {
+			return (phoneNumber);
+		}
+		std::string	getDarkestSecret() const {
+			return (darkestSecret);
+		}
+
+		void	setFirstName(const std::string& fName) {
+			firstName = fName;
+		}
+		void	setLastName(const std::string& lName) {
+			lastName = lName;
+		}
+		void	setNickName(const std::string& nName) {
+			nickName = nName;
+		}
+		void	setPhoneNumber(const std::string& phone) {
+			phoneNumber = phone;
+		}
+		void	setDarkestSecret(const std::string& secret) {
+			darkestSecret = secret;
+		}
+
+	private:
+		std::string		firstName;
+		std::string		lastName;
+		std::string		nickName;
+		std::string 	phoneNumber;
+		std::string		darkestSecret;
 };
 
-//class Phonebook {
-//	private:
-//		Contact	list[8];
-//
-//	public:
-//
-//
-//};
+class Phonebook {
+	public:
+//	Phonebook () {
+//		list = new Contact[LIST_SIZE];
+//	}
+
+		void	addContact() {
+			Contact newContact;
+			std::string line;
+
+			std::cout << "First name: ";
+			std::getline(std::cin, line);
+			newContact.setFirstName(line);
+
+			std::cout << "Last name: ";
+			std::getline(std::cin, line);
+			newContact.setLastName(line);
+
+			std::cout << "Nick name: ";
+			std::getline(std::cin, line);
+			newContact.setNickName(line);
+
+			std::cout << "Phone number: ";
+			std::getline(std::cin, line);
+			newContact.setPhoneNumber(line);
+
+			std::cout << "Darkest secret: ";
+			std::getline(std::cin, line);
+			newContact.setDarkestSecret(line);
+
+			std::cout << "\n-- Contact 1 --" << std::endl;
+			std::cout << "First name: " << newContact.getFirstName() << std::endl;
+			std::cout << "Last name: " << newContact.getLastName() << std::endl;
+			std::cout << "Nick name: " << newContact.getNickName() << std::endl;
+			std::cout << "Phone number: " << newContact.getPhoneNumber() << std::endl;
+			std::cout << "Darkest secret: " << newContact.getDarkestSecret() << std::endl << std::endl;
+//			list[0] = newContact;
+		}
+//		void	printContact() {
+//			std::cout << "\n-- Contact 1 --" << std::endl;
+//			std::cout << "First name: " << list[0].getFirstName() << std::endl;
+//			std::cout << "Last name: " << newContact.getLastName() << std::endl;
+//			std::cout << "Nick name: " << newContact.getNickName() << std::endl;
+//			std::cout << "Phone number: " << newContact.getPhoneNumber() << std::endl;
+//			std::cout << "Darkest secret: " << newContact.getDarkestSecret() << std::endl << std::endl;
+//		}
+
+//		void	sayName() {
+//			std::cout << "The first name of the first contact is: " << newContact.firstName << std::endl;
+//		}
+
+
+};
 
 #endif
